40af4230e3862cea1e79af0ab169f2d447bd7292
me <jarbas@xxx.com>
2018-09-10 00:13:06 -0300

    interface arrumada

--- src/main/java/edu/utfpr/cp/sa/gui/CustomerWindow.java
+++ src/main/java/edu/utfpr/cp/sa/gui/CustomerWindow.java
@@ -269,6 +269,8 @@ public class CustomerWindow extends JFrame {
 		
 		JButton btnDeletar = new JButton("Delete");
 		panel.add(btnDeletar);
+		//country = new JComboBox<>(new CountryDAO().findAll().stream().map(Country::getName).toArray(String[]::new));
+		panelInclusion.add(country);
 		
 		JButton btnAlterar = new JButton("Alterar");
 		panel.add(btnAlterar);
